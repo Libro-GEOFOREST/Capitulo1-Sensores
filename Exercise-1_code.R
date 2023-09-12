303e79b13e8e8ab3780b0438fa8865a7d550dd9c
Francisco J Ruiz-Gomez <78639099+RuizGomezFJ@users.noreply.github.com>
2023-09-12 19:11:47 +0200

    Update Exercise-1_code.R

--- Exercise-1_code.R
+++ Exercise-1_code.R
@@ -1,10 +1,11 @@
+## Ejecutar el código de las líneas 2 y 3 únicamente si no se han instalado antes las librerías.
 install.packages("tidyverse")
 install.packages("ggplot2")
 
 ## Cargamos las librerías y obtenemos los datos:
 library(tidyverse)
 library(ggplot2)
-url <- "https://raw.githubusercontent.com/RuizGomezFJ/GEOFOREST-Cchap1/Exercise-1/calibracion_TDR.csv"
+url <- "https://raw.githubusercontent.com/Libro-GEOFOREST/Capitulo1-Sensores/Exercise-1/calibracion_TDR.csv"
 datos <- read.csv(url)
 ## Resumen de los datos
 head(datos)
