137794842276988cbc26fb74a9e93c58e8d7447c
Francisco J Ruiz-Gomez <78639099+RuizGomezFJ@users.noreply.github.com>
2023-09-12 20:46:13 +0200

    Update Ejemplo2-code.R

--- Ejemplo2-code.R
+++ Ejemplo2-code.R
@@ -1,5 +1,12 @@
 ## Cargamos las librerías y obtenemos los datos:
 library(tidyverse)
 library(ggplot2)
-url <- "https://raw.githubusercontent.com/Libro-GEOFOREST/Capitulo1-Sensores/Ejemplo-2/fitdata_TDR.csv"
+url <- "https://raw.githubusercontent.com/Libro-GEOFOREST/Capitulo1-Sensores/Ejemplo-2/fitdata-TDR.csv"
 datos <- read.csv(url)
+fit <- as.data.frame(datos$fit)
+## Creamos una tabla de datos con las diferencias entre los pasos de voltaje
+dr_df <- as.data.frame(cbind(datos[-1,2],apply(fit,2,function(x) diff(x)/head(x,-1))))
+## Cambiar nombres para reconocer las variables
+colnames(dr_df) <- c("volt", "deltatita") 
+## Representación gráfica
+plot(dr_df$volt, dr_df$deltatita, xlab="mV",ylab=expression(delta))
