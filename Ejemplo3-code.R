f026fe023ff66bc9cf4bb257568ffa16bc8c86ce
Francisco J Ruiz-Gomez <78639099+RuizGomezFJ@users.noreply.github.com>
2023-09-13 20:23:51 +0200

    Update Ejemplo3-code.R

--- Ejemplo3-code.R
+++ Ejemplo3-code.R
@@ -1,8 +1,25 @@
-## Enfrentamos los datos esperados a los datos reales
-HAY QUE REVISAR ESTE EJEMPLO, LA FUNCION TIBBLE SIGUE SIN FUNCIONARME
-fitData2 <- tibble(x=datos$V,fit = 1/(a_param+(b_param*x)+(c_param*(x^2))+(d_param*(x^3))))
+########## GEOFOREST - Cap. 1 SENSORES / Ejemplo 3 ############################
+############ Francisco Jose Ruiz Gomez ########################################
 
-datos$esperados <- fitData2$fit
+## Cargamos las librerías y obtenemos los datos:
+library(tidyverse)
+library(ggplot2)
+url <- "https://raw.githubusercontent.com/Libro-GEOFOREST/Capitulo1-Sensores/Ejemplo-3/calibracion_TDR.csv"
+datos <- read.csv(url)
+
+## Enfrentamos los datos esperados a los datos reales para las 117 observaciones
+## Antes necesitamos nuevamente los parametros del modelo
+modelo <- lm(1/tita ~ V + I(V^2) + I(V^3) ,data=datos)
+
+## Extracción de los parámetros del modelo y obtencion de datos predichos
+a_param <- modelo$coefficients[[1]]
+b_param <- modelo$coefficients[[2]]
+c_param <- modelo$coefficients[[3]]
+d_param <- modelo$coefficients[[4]]
+fitData <- tibble(x=datos$V,fit = 1/(a_param+(b_param*x)+(c_param*(x^2))+(d_param*(x^3))))
+
+## Pareamos los datos esperados con los reales
+datos$esperados <- fitData$fit
 
 ggplot(data = datos, aes(x = tita, y = esperados))+
   geom_point()+
